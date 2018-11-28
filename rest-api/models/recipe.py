5e6210192b0b46f5ea27f149ae8b8fc46096be8c
pgarr <garlej.p@gmail.com>
2018-11-28 18:15:25 +0100

    set cascades for relations

--- rest-api/models/recipe.py
+++ rest-api/models/recipe.py
@@ -6,5 +6,5 @@ class Recipe(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    detail = db.relationship("RecipeDetail", uselist=False, back_populates="recipe")
-    ingredients = db.relationship('RecipeIngredient', lazy="dynamic")
+    detail = db.relationship("RecipeDetail", uselist=False, back_populates="recipe", cascade="all, delete-orphan")
+    ingredients = db.relationship('RecipeIngredient', lazy="dynamic", cascade="all, delete-orphan")
