8673bb0b3cca13950dc98deded3d62ad44c77bbe
pgarr <garlej.p@gmail.com>
2019-05-17 22:36:15 +0200

    changed message at startup

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -63,7 +63,7 @@ def create_app(config_class=Config):
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
-        app.logger.info('App startup')
+        app.logger.info('dinner-roulette startup')
 
     return app
 
