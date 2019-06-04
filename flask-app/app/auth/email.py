cc464ecd2955b4ad25aec6eef38fbb43bbfe97bf
pgarr <garlej.p@gmail.com>
2019-06-04 19:00:13 +0200

    marked all texts requiring translation

--- flask-app/app/auth/email.py
+++ flask-app/app/auth/email.py
@@ -1,11 +1,12 @@
 from flask import current_app, render_template
+from flask_babel import _
 
 from app.email import send_email
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Dinner-roulette] Reset Your Password',
+    send_email(_('[Dinner-roulette] Reset Your Password'),
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
--- flask-app/app/main/forms.py
+++ flask-app/app/main/forms.py
@@ -1,3 +1,4 @@
+from flask_babel import lazy_gettext as _l
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, IntegerField, FormField, \
     FieldList, FloatField
@@ -5,21 +6,21 @@ from wtforms.validators import URL, NumberRange, Optional, Length
 
 
 class IngredientForm(FlaskForm):
-    title = StringField('Ingredient name', validators=[Optional(), Length(max=100)])
-    amount = FloatField('Ingredient amount', validators=[Optional(), NumberRange(1, 999)])
-    unit = StringField('Ingredient unit',
+    title = StringField(_l('Ingredient name'), validators=[Optional(), Length(max=100)])
+    amount = FloatField(_l('Ingredient amount'), validators=[Optional(), NumberRange(1, 999)])
+    unit = StringField(_l('Ingredient unit'),
                        validators=[Optional(), Length(max=20)])  # TODO: w formularzu dać podpowiedzi z istniejących
 
 
 class RecipeForm(FlaskForm):
-    title = StringField('Recipe title', validators=[Length(min=3, max=200)])
-    time = IntegerField('Preparation time (minutes)', validators=[Optional(), NumberRange(1, 999)])
-    difficulty = IntegerField('Preparation difficulty (1-5)',
+    title = StringField(_l('Recipe title'), validators=[Length(min=3, max=200)])
+    time = IntegerField(_l('Preparation time (minutes)'), validators=[Optional(), NumberRange(1, 999)])
+    difficulty = IntegerField(_l('Preparation difficulty (1-5)'),
                               validators=[Optional(), NumberRange(1, 5)])  # TODO: Powiązać z ładnym selectorem
     # difficulty = SelectField('Preparation difficulty', choices=[(c, c) for c in [1, 2, 3, 4, 5]])
-    link = StringField('Website', validators=[Optional(), URL()])
-    preparation = TextAreaField('Preparation', validators=[Optional(), Length(max=10000)])
+    link = StringField(_l('Website'), validators=[Optional(), URL()])
+    preparation = TextAreaField(_l('Preparation'), validators=[Optional(), Length(max=10000)])
     ingredients = FieldList(FormField(IngredientForm), min_entries=1)
     add_ingredient = SubmitField('+')
     remove_ingredient = SubmitField('-')
-    submit = SubmitField('Submit')
+    submit = SubmitField(_l('Submit'))
