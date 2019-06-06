a8974b93d0e19d37bf7e2ae4ac5470edb4c58a5c
pgarr <garlej.p@gmail.com>
2019-06-06 23:07:02 +0200

    fix for logging

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -64,18 +64,19 @@ def create_app(config_class=Config):
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/dinner-roulette.log', maxBytes=102400, backupCount=100)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s '
-            '[in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/dinner-roulette.log', maxBytes=102400, backupCount=100)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
         app.logger.setLevel(logging.INFO)
         app.logger.info('dinner-roulette startup')
 
-        print(app.config['APP_ADMINS'])
     return app
 
 
