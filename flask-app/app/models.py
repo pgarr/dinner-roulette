4c7ca74d794b412a04d82cd463efc2847372fa9d
pgarr <garlej.p@gmail.com>
2019-06-19 18:40:45 +0200

    removed unused admin property

--- flask-app/app/models.py
+++ flask-app/app/models.py
@@ -15,7 +15,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    admin = db.Column(db.Boolean, default=False)
     password_hash = db.Column(db.String(128))
 
     @property
