82b770a7f2634954f24991fdc78df3c98458e1da
pgarr <garlej.p@gmail.com>
2019-06-30 13:31:13 +0200

    Login Page Model

--- selenium-tests/models/locators.py
+++ selenium-tests/models/locators.py
@@ -20,4 +20,9 @@ class HomePageLocators(BasePageLocators):
 
 
 class LoginPageLocators(BasePageLocators):
-    pass
+    USERNAME_FIELD = (By.ID, 'username')
+    PASSWORD_FIELD = (By.ID, 'password')
+    REMEMBER_ME_CHECKBOX = (By.ID, 'remember_me')
+    SUBMIT_BUTTON = (By.ID, 'submit')
+    NEW_USER_BUTTON = (By.XPATH, '/html/body/div[2]/div[2]/a[1]')
+    FORGOTTEN_PASSWORD_BUTTON = (By.XPATH, '/html/body/div[2]/div[2]/a[2]')
--- selenium-tests/models/pages.py
+++ selenium-tests/models/pages.py
@@ -1,7 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 
-from errors import NotLoggedInError, UserLoggedInError
-from models.locators import BasePageLocators
+from utils.errors import NotLoggedInError, UserLoggedInError
+from models.locators import BasePageLocators, LoginPageLocators
 
 
 class BasePage:
@@ -31,36 +31,36 @@ class BasePage:
         """Visible if user is not logged in"""
         try:
             return self.driver.find_element(*BasePageLocators.LOGIN_BUTTON)
-        except NoSuchElementException:
-            raise UserLoggedInError
+        except NoSuchElementException as e:
+            raise UserLoggedInError from e
 
     @property
     def user_menu_dropdown(self):
         """Visible for logged in user"""
         try:
             return self.driver.find_element(*BasePageLocators.USER_MENU_DROPDOWN)
-        except NoSuchElementException:
-            raise NotLoggedInError
+        except NoSuchElementException as e:
+            raise NotLoggedInError from e
 
     @property
     def waiting_recipes_button(self):
         """Visible for logged in user"""
         try:
             return self.driver.find_element(*BasePageLocators.WAITING_RECIPES_BUTTON)
-        except NoSuchElementException:
-            raise NotLoggedInError()
+        except NoSuchElementException as e:
+            raise NotLoggedInError from e
 
     @property
     def logout_button(self):
         """Visible for logged in user"""
         try:
             return self.driver.find_element(*BasePageLocators.LOGOUT_BUTTON)
-        except NoSuchElementException:
-            raise NotLoggedInError()
+        except NoSuchElementException as e:
+            raise NotLoggedInError from e
 
     @property
     def user_name(self):
-        return self.user_menu_dropdown.text()
+        return self.user_menu_dropdown.text
 
     def is_url_correct(self):
         return self.driver.current_url == self.url
@@ -78,7 +78,7 @@ class HomePage(BasePage):
 
     @property
     def url(self):
-        return super().url + '/'
+        return super().url + '/index'
 
 
 class LoginPage(BasePage):
@@ -86,3 +86,33 @@ class LoginPage(BasePage):
     @property
     def url(self):
         return super().url + '/auth/login'
+
+    @property
+    def username_field(self):
+        return self.driver.find_element(*LoginPageLocators.USERNAME_FIELD)
+
+    @property
+    def password_field(self):
+        return self.driver.find_element(*LoginPageLocators.PASSWORD_FIELD)
+
+    @property
+    def remember_me_checkbox(self):
+        return self.driver.find_element(*LoginPageLocators.REMEMBER_ME_CHECKBOX)
+
+    @property
+    def submit_button(self):
+        return self.driver.find_element(*LoginPageLocators.SUBMIT_BUTTON)
+
+    @property
+    def new_user_button(self):
+        return self.driver.find_element(*LoginPageLocators.NEW_USER_BUTTON)
+
+    @property
+    def forgotten_password_button(self):
+        return self.driver.find_element(*LoginPageLocators.FORGOTTEN_PASSWORD_BUTTON)
+
+    def login(self, username, password):
+        self.username_field.send_keys(username)
+        self.password_field.send_keys(password)
+        self.submit_button.click()
+        return HomePage(self.driver)
