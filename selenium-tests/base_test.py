2a81e58c3ca8d813200643dba18e27400bff8ff2
pgarr <garlej.p@gmail.com>
2019-10-10 20:22:56 +0200

    aut run moved to setup class

--- selenium-tests/base_test.py
+++ selenium-tests/base_test.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 
 from config import MAX_LOADING_TIME
@@ -12,13 +12,15 @@ from utils.aut import Aut
 
 
 class BaseTest(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         basedir = os.path.abspath(os.path.dirname(__file__))
         load_dotenv(os.path.join(basedir, '.env'))
         # run aut
-        self.aut = Aut()
-        self.aut.run(10)
+        cls._aut = Aut()
+        cls._aut.run(10)
 
+    def setUp(self):
         # set language preferences
         options = webdriver.ChromeOptions()
         options.add_argument('--lang=en')
@@ -30,7 +32,10 @@ class BaseTest(TestCase):
 
     def tearDown(self):
         self.driver.quit()
-        self.aut.stop()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._aut.stop()
 
     def wait_page_changes(self, current_page: BasePage, expected_page: BasePage):
         try:
@@ -39,4 +44,3 @@ class BaseTest(TestCase):
             self.fail("Loading timeout expired")
         self.assertTrue(expected_page.is_title_correct())
         return expected_page
-
---
+++ selenium-tests/test_cases/test_access_pages.py
@@ -0,0 +1,2 @@
+# reqiures some created recipes and waitng recipes, check if user can edit someone else recipe,
+# if standard user can acccept waiting recipe, etc.
