cadaa9e075e842fb01b0b7f6ed2569846cfb9316
pgarr <garlej.p@gmail.com>
2019-10-23 19:39:59 +0200

    removed . and fixed redirect after login

--- flask-app/app/auth/routes.py
+++ flask-app/app/auth/routes.py
@@ -22,7 +22,7 @@ def login():
             flash(_('Invalid username or password'))
             return redirect(url_for('.login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('.next')
+        next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
