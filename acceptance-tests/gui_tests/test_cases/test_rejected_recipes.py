3db2c98a0b6cbf0243eae8058b2c64a418d553c4
pgarr <garlej.p@gmail.com>
2020-02-17 19:52:48 +0100

    test_new_updates_overrides_rejected_updates (#31)

--- acceptance-tests/gui_tests/test_cases/test_rejected_recipes.py
+++ acceptance-tests/gui_tests/test_cases/test_rejected_recipes.py
@@ -1,6 +1,7 @@
 from gui_tests.base_test import BaseTest
 from gui_tests.helpers import wait_page_changes
-from gui_tests.models.pages import HomePage, WaitingRecipesPage
+from gui_tests.models.pages import HomePage, WaitingRecipesPage, RecipePage, WaitingRecipePage, \
+    EditWaitingRecipePage
 
 
 class RejectedRecipesTest(BaseTest):
@@ -34,8 +35,7 @@ class RejectedRecipesTest(BaseTest):
         self.navbar.go_to_waiting_page()
         waiting_recipes_page = wait_page_changes(home_page, WaitingRecipesPage(self.driver))
 
-        self.assertEqual(len(waiting_recipes_page.recipes), 2,
-                         msg="Waiting recipes count is correct")
+        self.assertEqual(len(waiting_recipes_page.recipes), 2)
 
     def test_admin_dont_see_rejected_recipe_on_list(self):
         home_page = HomePage(self.driver)
@@ -46,11 +46,31 @@ class RejectedRecipesTest(BaseTest):
         self.navbar.go_to_waiting_page()
         waiting_recipes_page = wait_page_changes(home_page, WaitingRecipesPage(self.driver))
 
-        self.assertEqual(len(waiting_recipes_page.recipes), 1,
-                         msg="Waiting recipes count is correct")
+        self.assertEqual(len(waiting_recipes_page.recipes), 1)
 
     def test_new_updates_overrides_rejected_updates(self):
-        self.fail()
+        home_page = HomePage(self.driver)
+        self.driver.get(home_page.url)
+
+        self.navbar.smart_login(home_page, 'test', 'test')
+
+        home_page.recipes[0].go_to_details()
+        recipe_page = wait_page_changes(home_page, RecipePage(self.driver))
+
+        recipe_page.edit()
+        edit_waiting_page = wait_page_changes(recipe_page, EditWaitingRecipePage(self.driver))
+
+        self.assertEqual(edit_waiting_page.name.get_text(), 'rejected')
+
+        edit_waiting_page.name.set_text('new_name')
+        edit_waiting_page.submit()
+        waiting_recipe_page = wait_page_changes(edit_waiting_page, WaitingRecipePage(self.driver))
+
+        self.navbar.go_to_waiting_page()
+        waiting_recipes_page = wait_page_changes(waiting_recipe_page, WaitingRecipesPage(self.driver))
+
+        self.assertEqual(len(waiting_recipes_page.recipes), 2)
+        self.assertEqual(waiting_recipes_page.recipes[1].name, 'new_name')
 
     def test_rejecting_recipe(self):
         self.fail()
