87e7c6bc472545f038b7267a58e71f71f1c5006b
pgarr <garlej.p@gmail.com>
2020-11-18 20:00:45 +0100

    custom hook for fetching api

--- react-app/src/components/Recipes/RecipesList/RecipesList.js
+++ react-app/src/components/Recipes/RecipesList/RecipesList.js
@@ -1,50 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import axios from "../../../shared/axios-api";
+import { useFetchApi } from "../../../shared/customHooks";
 import NumberedPagination from "../../UI/NumberedPagination/NumberedPagination";
 import RecipesTable from "./RecipesTable/RecipesTable";
 
 const RecipesList = ({ history }) => {
-  const [activePage, setActivePage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [recipes, setRecipes] = useState([]);
-
-  const [isLoading, setIsLoading] = useState(false); //TODO
-  const [isError, setIsError] = useState(false); //TODO
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-
-      try {
-        const response = await axios.get("/recipes?page=" + activePage);
-        setRecipes(response.data.recipes);
-        setTotalPages(response.data._meta.total_pages);
-      } catch (error) {
-        console.log(error);
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [activePage]);
+  const [{ data, isLoading, isError }, doFetch] = useFetchApi("/recipes", {
+    recipes: [],
+    _meta: { page: 1, total_pages: 1 },
+  });
 
   const recipeSelectedHandler = (id) => {
     history.push({ pathname: "/recipes/" + id });
   };
 
   const pageChangedHandler = (page) => {
-    setActivePage(page);
+    doFetch(`/recipes?page=${page}`);
   };
 
   return (
     <React.Fragment>
-      <RecipesTable recipes={recipes} onSelectRecipe={recipeSelectedHandler} />
+      <RecipesTable
+        recipes={data.recipes}
+        onSelectRecipe={recipeSelectedHandler}
+      />
       <NumberedPagination
-        activePage={activePage}
-        totalPages={totalPages}
+        activePage={data._meta.page}
+        totalPages={data._meta.total_pages}
         onChangePage={pageChangedHandler}
       />
     </React.Fragment>
--- react-app/src/shared/customHooks.js
+++ react-app/src/shared/customHooks.js
@@ -1,4 +1,6 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+import axios from "./axios-api";
 
 export const useDebouncedEffect = (effect, delay, deps) => {
   const callback = useCallback(effect, deps);
@@ -12,4 +14,32 @@ export const useDebouncedEffect = (effect, delay, deps) => {
       clearTimeout(handler);
     };
   }, [callback, delay]);
+};
+
+export const useFetchApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios.get(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
 };
